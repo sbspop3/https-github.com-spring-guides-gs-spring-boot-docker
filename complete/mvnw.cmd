9f8285bb8f57eef7786f7d1db20e8259f6f35a1f
sonal25a <45196844+sonal25a@users.noreply.github.com>
2019-12-03 05:32:44 +0530

    Update mvnw.cmd

--- complete/mvnw.cmd
+++ complete/mvnw.cmd
@@ -69,6 +69,7 @@ goto error
 if exist "%JAVA_HOME%\bin\java.exe" goto init
 
 echo.
+echo.
 echo Error: JAVA_HOME is set to an invalid directory. >&2
 echo JAVA_HOME = "%JAVA_HOME%" >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
